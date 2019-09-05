ad1deaa929d280101a9c2e534a7a71e0b0d39b7a
Jarrett Green <jarrett_green@outlook.com>
2019-09-05 15:43:20 -0500

    Update Patient.cs

--- County-OR-Board.Domain/Models/Patient.cs
+++ County-OR-Board.Domain/Models/Patient.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using County_OR_Board.Domain.Abstracts;
 
 namespace County_OR_Board.Domain.Models
 {
     public class Patient : AUser
     {
-
+        [Required]
+        public string Gender { get; set; }
     }
 }
