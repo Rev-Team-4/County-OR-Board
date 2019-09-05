dc354eb4e47b7634fc427829d7f6bd2eeaf39a91
Jarrett Green <jarrett_green@outlook.com>
2019-09-05 13:44:27 -0500

    Added Properties

--- County-OR-Board.Domain/Models/Procedure.cs
+++ County-OR-Board.Domain/Models/Procedure.cs
@@ -1,4 +1,6 @@
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace County_OR_Board.Domain
 {
@@ -6,5 +8,42 @@ namespace County_OR_Board.Domain
     {
         [Key]
         public int ProcedureId { get; set; }
+
+        [ForeignKey("SurgeonId")]
+        public int SurgeonId { get; set; }
+
+        [ForeignKey("AnesthetistId")]
+        public int AnesthetistId { get; set; }
+
+        [ForeignKey("OperatingRoomId")]
+        public int OperatingRoomId { get; set; }
+
+        [ForeignKey("HospitalId")]
+        public int HospitalId { get; set; }
+
+        [ForeignKey("Patient")]
+        public int PatientId { get; set; }
+
+        [ForeignKey("NurseId")]
+        public int NurseId { get; set; }
+
+        [Required]
+        public DateTime ScheduledDateTime { get; set; }
+        
+        [Required]
+        public string ProcedureName { get; set; }
+       
+        [Required]
+        public string Status { get; set; }
+       
+        [Required]
+        public DateTime EstimatedProcedureLength { get; set; }
+      
+        [Required]
+        public DateTime ActualStart { get; set; }
+       
+        [Required]
+        public DateTime ActualFinish { get; set; }
+
     }
 }
