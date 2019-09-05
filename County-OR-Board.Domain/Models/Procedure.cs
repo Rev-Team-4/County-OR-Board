1554a993df9551dbccca6662773a6c160be796ad
Jarrett Green <jarrett_green@outlook.com>
2019-09-05 13:16:44 -0500

    Added entity framework core

--- County-OR-Board.Domain/Models/Procedure.cs
+++ County-OR-Board.Domain/Models/Procedure.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace County_OR_Board.Domain
 {
     class Procedure
     {
-        
+        [Key]
+        public int ProcedureId { get; set; }
     }
 }
