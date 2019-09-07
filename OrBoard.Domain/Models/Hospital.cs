8091fed5020504a7deb5fb415973f4b438cc536e
Jarrett Green <jarrett_green@outlook.com>
2019-09-07 17:54:13 -0500

    Revert "Merge branch 'felix' of https://github.com/Rev-Team-4/County-OR-Board"
    This reverts commit ac0de69a4d61b16d2c0981c06504f5482af9db97, reversing
    changes made to 25841210cfb4a0eb1ae62cc7f0ab12120441cd78.

--- OrBoard.Domain/Models/Hospital.cs
+++ OrBoard.Domain/Models/Hospital.cs
@@ -5,53 +5,53 @@ namespace OrBoard.Domain.Models
 {
     public class Hospital
     {
-        [Key]
-        public int HospitalId { get; set; }
+      [Key]
+      public int HospitalId {get; set;}
 
-        [Required]
-        [MaxLength(50)]
-        public string Name { get; set; }
+      [Required]
+      [MaxLength(50)]
+      public string Name {get; set;}
 
-        [Required]
-        [MaxLength(50)]
-        public string Address { get; set; }
+      [Required]
+      [MaxLength(50)]
+      public string Address {get; set;}
 
-        [Required]
-        [MaxLength(50)]
-        public string FacilityOwnership { get; set; }
+      [Required]
+      [MaxLength(50)]
+      public string FacilityOwnership {get; set;}
 
-        [Required]
-        [MaxLength(50)]
-        public string ORManager { get; set; }
+      [Required]
+      [MaxLength(50)]
+      public string ORManager {get; set;}
 
-        [Required]
-        [MaxLength(50)]
-        public string ManagerEmail { get; set; }
+      [Required]
+      [MaxLength(50)]
+      public string ManagerEmail {get; set;}
 
-        [Required]
-        [Range(0, int.MaxValue)]
-        public int LicenseNumber { get; set; }
+      [Required]
+      [Range(0, int.MaxValue)]
+      public int LicenseNumber {get; set;}
 
-        [Required]
-        public bool LicenseStatus { get; set; }
+      [Required]
+      public bool LicenseStatus {get; set;}
 
-        [Required]
-        [Range(0, int.MaxValue)]
-        public int EINNumber { get; set; }
+      [Required]
+      [Range(0, int.MaxValue)]
+      public int EINNumber {get; set;}
 
-        [Required]
-        public bool EINStatus { get; set; }
+      [Required]
+      public bool EINStatus {get; set;}
 
-        [Required]
-        [MaxLength(50)]
-        public string FacilityType { get; set; }
+      [Required]
+      [MaxLength(50)]
+      public string FacilityType {get; set;}
 
-        [Required]
-        [MaxLength(50)]
-        public string City { get; set; }
+      [Required]
+      [MaxLength(50)]
+      public string City {get; set;}
 
-        [Required]
-        [MaxLength(50)]
-        public string State { get; set; }
+      [Required]
+      [MaxLength(50)]
+      public string State {get; set;}
     }
 }
