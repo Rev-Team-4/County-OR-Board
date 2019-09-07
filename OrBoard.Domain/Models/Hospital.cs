8067c30a07222598a6887c1230985a869bc59068
felixwafula <felix.a.wafula@gmail.com>
2019-09-07 16:40:30 -0500

    Hospital and DBcontext

--- OrBoard.Domain/Models/Hospital.cs
+++ OrBoard.Domain/Models/Hospital.cs
@@ -5,70 +5,53 @@ namespace OrBoard.Domain.Models
 {
     public class Hospital
     {
-<<<<<<< HEAD
         [Key]
         public int HospitalId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Address { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string FacilityOwnership { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string ORManager { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string ManagerEmail { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
         public int LicenseNumber { get; set; }
+
+        [Required]
         public bool LicenseStatus { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
         public int EINNumber { get; set; }
+
+        [Required]
         public bool EINStatus { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string FacilityType { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string City { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string State { get; set; }
-=======
-      [Key]
-      public int HospitalId {get; set;}
-
-      [Required]
-      [MaxLength(50)]
-      public string Name {get; set;}
-
-      [Required]
-      [MaxLength(50)]
-      public string Address {get; set;}
-
-      [Required]
-      [MaxLength(50)]
-      public string FacilityOwnership {get; set;}
-
-      [Required]
-      [MaxLength(50)]
-      public string ORManager {get; set;}
-
-      [Required]
-      [MaxLength(50)]
-      public string ManagerEmail {get; set;}
-
-      [Required]
-      [Range(0, int.MaxValue)]
-      public int LicenseNumber {get; set;}
-
-      [Required]
-      public bool LicenseStatus {get; set;}
-
-      [Required]
-      [Range(0, int.MaxValue)]
-      public int EINNumber {get; set;}
-
-      [Required]
-      public bool EINStatus {get; set;}
-
-      [Required]
-      [MaxLength(50)]
-      public string FacilityType {get; set;}
-
-      [Required]
-      [MaxLength(50)]
-      public string City {get; set;}
-
-      [Required]
-      [MaxLength(50)]
-      public string State {get; set;}
->>>>>>> 8995f272230766b07a0fb0811444c3e0d6715f11
     }
 }
