5d0bd0adc88e02bbab8d176854aac59971b59ee3
felixwafula <felix.a.wafula@gmail.com>
2019-09-07 17:09:47 -0500

    adddDbContext

---
+++ OrBoard.Data/OrBoardDbContext.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using OrBoard.Domain.Models;
+
+namespace OrBoard.Data
+{
+    public class OrBoardDbContext : DbContext
+    {
+
+        public DbSet<Anesthetist> Anesthetists { get; set; }
+        public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<Login> Logins { get; set; }
+        public DbSet<Nurse> Nurses { get; set; }
+        public DbSet<OperatingRoom> OperatingRooms { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Procedure> Procedures { get; set; }
+        public DbSet<Surgeon> Surgeons { get; set; }
+        
+
+        public OrBoardDbContext()
+        {
+
+        }
+
+        public OrBoardDbContext(DbContextOptions<OrBoardDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("server=localhost; initial catalog=OrBoardDb; user id=sa; password=Password12345");
+        }
+    }
+}
