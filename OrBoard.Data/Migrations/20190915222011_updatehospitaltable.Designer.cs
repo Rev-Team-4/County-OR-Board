7e2d14a571f5868834e6124e20c16f44ed840dff
Jarrett Green <jarrett_green@outlook.com>
2019-09-15 18:11:20 -0500

    update tables and layout

--- OrBoard.Data/Migrations/20190915222011_updatehospitaltable.Designer.cs
+++ OrBoard.Data/Migrations/20190915222011_updatehospitaltable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrBoard.Data;
 
 namespace OrBoard.Data.Migrations
 {
     [DbContext(typeof(OrBoardDbContext))]
-    partial class OrBoardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190915222011_updatehospitaltable")]
+    partial class updatehospitaltable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OrBoard.Data/Migrations/20190915222011_updatehospitaltable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrBoard.Data.Migrations
+{
+    public partial class updatehospitaltable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Fax",
+                table: "Hospitals",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telephone",
+                table: "Hospitals",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ZipCode",
+                table: "Hospitals",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fax",
+                table: "Hospitals");
+
+            migrationBuilder.DropColumn(
+                name: "Telephone",
+                table: "Hospitals");
+
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "Hospitals");
+        }
+    }
+}
